670cb17668058e86c5d5fcf186da8629fcb681ee
SantiagoYakowitz <76271734+SantiagoYakowitz@users.noreply.github.com>
2021-08-13 15:36:07 -0300

    Indentación de espacios, error al ingresar >=976

--- Codigo_ejemplo2_TP_Digitales.asm
+++ Codigo_ejemplo2_TP_Digitales.asm
@@ -1,97 +1,97 @@
 ; Alumno: Yakowitz, Santiago. 2021
 
 datos segment
-	diez db 10
-	cien db 100
-	resto db 0
-	array dw 10 dup(?) ; Array de maximo 10 numeros
-	c dw 0
-	msg db "Ingrese al menos 2 numeros (0-999), ENTER para ingresar y ESC para terminar:$"
-	num_count db 0  ; Cantidad de numeros ingresados en tabla
-	num dw 0
-	num_hex db 0    ; Numero ingresado en hex (ascii) para mostrarlo en pantalla (30h - 39h)
-	db 36
-	cont db 0       ; Cuenta los digitos del numero actual
-	pos db 1        ; Fila en la pantalla
+    diez db 10
+    cien db 100
+    resto db 0
+    array dw 10 dup(?) ; Array de maximo 10 numeros
+    c dw 0
+    msg db "Ingrese al menos 2 numeros (0-999), ENTER para ingresar y ESC para terminar:$"
+    num_count db 0  ; Cantidad de numeros ingresados en tabla
+    num dw 0
+    num_hex db 0    ; Numero ingresado en hex (ascii) para mostrarlo en pantalla (30h - 39h)
+    db 36
+    cont db 0       ; Cuenta los digitos del numero actual
+    pos db 1        ; Fila en la pantalla
 datos ends
 
 pila segment stack
-	dw 128 dup (?)
-	cab_pila label word
+    dw 128 dup (?)
+    cab_pila label word
 pila ends
 
 prog segment stack
 assume cs: prog, ds: datos, ss: pila
 
 start:
-	mov bx, datos
-	mov ds, bx
-	mov bx, pila
-	mov ss, bx
-	lea sp, cab_pila
+    mov bx, datos
+    mov ds, bx
+    mov bx, pila
+    mov ss, bx
+    lea sp, cab_pila
 
-	lea bx, array          ; Cargo la direc efec del array en bx
-    
+    lea bx, array          ; Cargo la direc efec del array en bx
+
     lea dx, msg          ; Muestro el titulo
     call print_str       
-    
+
     ; --- Ingreso de los numeros ---
-	new_num:
-		mov cont, 00       ; Inicio variables
-		mov num, 00
-		mov dh, pos        ; fila
-		mov dl, 0          ; columna
-		mov ah, 02
-		mov bh, 00         ; muevo el cursor
-		int 10h
+    new_num:
+        mov cont, 00       ; Inicio variables
+        mov num, 00
+        mov dh, pos        ; fila
+        mov dl, 0          ; columna
+        mov ah, 02
+        mov bh, 00         ; muevo el cursor
+        int 10h
 
-	key_input:
-		call input_key     ; La tecla se registra en al
-		cmp al, 13         ; al == 13: Enter
-		je enter
-		cmp al, 27         ; al == 27: Esc
-		je esc
-		
-		; 0 - 9 = 30h - 39h
-		cmp al, 30h        ; al < 30h: volver a pedir tecla
-		jl key_input
-		cmp al, 39h
-		jg key_input       ; al > 39h: volver a pedir tecla
+    key_input:
+        call input_key     ; La tecla se registra en al
+        cmp al, 13         ; al == 13: Enter
+        je enter
+        cmp al, 27         ; al == 27: Esc
+        je esc
+        
+        ; 0 - 9 = 30h - 39h
+        cmp al, 30h        ; al < 30h: volver a pedir tecla
+        jl key_input
+        cmp al, 39h
+        jg key_input       ; al > 39h: volver a pedir tecla
 
-		inc cont           ; Sumamos un nuevo digito
+        inc cont           ; Sumamos un nuevo digito
 
-		mov num_hex, al    ; Muevo el valor leido en al a num_hex
-		lea dx, num_hex    ; y cargo la direc efectiva en dx para mostrarlo en pantalla
-		call print_str
+        mov num_hex, al    ; Muevo el valor leido en al a num_hex
+        lea dx, num_hex    ; y cargo la direc efectiva en dx para mostrarlo en pantalla
+        call print_str
 
-		mov ax, num
-		mul diez           ; Multiplico por diez para agregar la unidad, decena o centena
-		mov dl, num_hex    ; Ingreso el numero en dx (dh: 00, dl: 30 - 39)
-		mov dh, 00
-		add ax, dx         ; Sumo este numero con ax
-		sub al, 30h        ; Le resto 30h para obtener el numero real
-		mov num, ax        ; Lo guardo en num
-		cmp cont, 03       ; Tomamos como que se ingreso enter si se ingresan 3 digitos
-		je enter
-		jmp key_input      ; Sino, volver a pedir otro digito
+        mov ax, num
+        mul diez           ; Multiplico por diez para agregar la unidad, decena o centena
+        mov dl, num_hex    ; Ingreso el numero en dx (dh: 00, dl: 30 - 39)
+        mov dh, 00
+        sub dx, 30         ; Le resto 30h para obtener el numero real
+        add ax, dx         ; Sumo este numero con ax
+        mov num, ax        ; Lo guardo en num
+        cmp cont, 03       ; Tomamos como que se ingreso enter si se ingresan 3 digitos
+        je enter
+        jmp key_input      ; Sino, volver a pedir otro digito
 
-	enter:
-		cmp cont, 00       ; Si no se ingreso ningun numero, volver a pedir
-		je key_input
-		inc pos            ; Sumamos fila del cursor
-		inc num_count      ; Sumamos numeros ingresados
-		mov ax, num        ; Lo ingresamos en el array
-		mov [bx], ax
-		add bx, 2          ; Pasamos al siguiente elemento en el array
-		cmp num_count, 10  ; Verificar si se llego al limite
-		jne new_num
-           
-	esc:
-		cmp pos, 02        ; Si numeros ingresados <= 1: volver a pedir
-		jbe key_input
-	
-	; --- Ordenamiento del array ---
-	; Ver explicacion del funcionamiento al final del programa
+    enter:
+        cmp cont, 00       ; Si no se ingreso ningun numero, volver a pedir
+        je key_input
+        inc pos            ; Sumamos fila del cursor
+        inc num_count      ; Sumamos numeros ingresados
+        mov ax, num        ; Lo ingresamos en el array
+        mov [bx], ax
+        add bx, 2          ; Pasamos al siguiente elemento en el array
+        cmp num_count, 10  ; Verificar si se llego al limite
+        jne new_num
+
+    esc:
+        cmp pos, 02        ; Si numeros ingresados <= 1: volver a pedir
+        jbe key_input
+    
+    ; --- Ordenamiento del array ---
+    ; Ver explicacion del funcionamiento al final del programa
     mov ah, 00
     mov al, num_count ; ax contiene la cantidad de numeros ingresados
     mov bl, 02
@@ -124,65 +124,65 @@ start:
         dec c
         cmp bx, 01 ; El primer bucle termina cuando bx == 1
         jg loop1
-	
-	; --- Mostrar el array ---
-	lea bx, array
-	mov pos, 01            ; Empezamos en la fila 1
+    
+    ; --- Mostrar el array ---
+    lea bx, array
+    mov pos, 01            ; Empezamos en la fila 1
 
-	print:
-	    mov dh, pos        ; Fila
-	    mov dl, 25         ; Columna
-	    mov ah, 02         ; Muevo cursor
-	    mov bh, 00
-	    int 10h
-	    
-	    mov ax, [bx]       ; Cargo el numero del array en ax
-	    
-	    cmp ax, 100        ; Hago el salto correspondiente si es mayor o igual a 100
-	    jae centena        
-	    cmp ax, 10         ; o mayor o igual a 10
-	    jae decena
-	    jmp unidad 
+    print:
+        mov dh, pos        ; Fila
+        mov dl, 25         ; Columna
+        mov ah, 02         ; Muevo cursor
+        mov bh, 00
+        int 10h
+        
+        mov ax, [bx]       ; Cargo el numero del array en ax
 
-	    centena:
-	        div cien       ; entero: al, resto: ah
-	        mov resto, ah  ; Guardo el resto para usarlo en la decena
-	        add al, 30h    ; Lo paso a su correspondiente ascii
-	        mov dl, al     ; Lo cargo a dl y lo muestro
-	        call print_char
-	    
-	    mov ah, 00         ; Reseteo las variables y prosigo de la misma forma
-	    mov al, resto
-	    
-	    decena:
-	        div diez
-	        mov resto, ah
-	        add al, 30h
-	        mov dl, al
-	        call print_char
-	    
-	    mov ah, 00
-	    mov al, resto
-	    
-	    unidad:
-	        add al, 30h
-	        mov dl, al
-	        call print_char
-	                        
-	    add bx, 02          ; Paso al siguiente numero en el array
-	    inc pos             ; Paso a la siguiente fila
-	    dec num_count       ; Decremento la cantidad de numeros
-	    cmp num_count, 00   ; Compruebo si se mostraron todos los numeros
-	    je exit
-	    jmp print
-	prog ends
+        cmp ax, 100        ; Hago el salto correspondiente si es mayor o igual a 100
+        jae centena        
+        cmp ax, 10         ; o mayor o igual a 10
+        jae decena
+        jmp unidad 
+
+        centena:
+            div cien       ; entero: al, resto: ah
+            mov resto, ah  ; Guardo el resto para usarlo en la decena
+            add al, 30h    ; Lo paso a su correspondiente ascii
+            mov dl, al     ; Lo cargo a dl y lo muestro
+            call print_char
+        
+        mov ah, 00         ; Reseteo las variables y prosigo de la misma forma
+        mov al, resto
+        
+        decena:
+            div diez
+            mov resto, ah
+            add al, 30h
+            mov dl, al
+            call print_char
+        
+        mov ah, 00
+        mov al, resto
+
+        unidad:
+            add al, 30h
+            mov dl, al
+            call print_char
+
+        add bx, 02          ; Paso al siguiente numero en el array
+        inc pos             ; Paso a la siguiente fila
+        dec num_count       ; Decremento la cantidad de numeros
+        cmp num_count, 00   ; Compruebo si se mostraron todos los numeros
+        je exit
+        jmp print
+    prog ends
 
     ; --- Funciones utiles ---
-    
+
     ; Ingreso de numero por teclado
     input_key:
         mov ah, 00
-	    int 16h    
+        int 16h    
         ret
     
     ; Mostrar una cadena de caracteres
